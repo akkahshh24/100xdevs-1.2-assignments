d88e3632bb51fb77cf8c07923bb7241c715f5252
Akkahshh24 <akkahshh24@gmail.com>
2023-12-18 12:43:53 +0530

    solve fileServer.js

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -16,6 +16,38 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const port = 3000;
 
+app.get("/files", (req, res) => {
+  fs.readdir(path.join(__dirname, './files'), (err, files) => {
+    if (err) {
+      return res.status(500).json({
+        error: "Failed to read files"
+      });
+    }
+    res.json(files);
+  });
+});
+
+app.get("/file/:filename", (req, res) => {
+  const filePath = path.join(__dirname, './files/', req.params.filename);
+
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      return res.status(404).send("File not found");
+    }
+    res.send(data);
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    error: "Page not found"
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
 
 module.exports = app;
