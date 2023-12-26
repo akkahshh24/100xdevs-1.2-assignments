e6da478858ed2ce9d8841980ef1c29ef755e0004
akkahshh24 <akkahshh24@gmail.com>
2023-12-26 22:04:10 +0530

    attempt mongo db assignment

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -5,14 +5,37 @@ const router = Router();
 // Admin Routes
 router.post('/signup', (req, res) => {
     // Implement admin signup logic
+    Admin.create({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    res.json({
+        message: "Admin created successfully"
+    });
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
     // Implement course creation logic
+    Course.create({
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price,
+        image: req.body.image
+    });
+
+    res.json({
+        message: "Course created successfully",
+        courseId: ""
+    });
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
     // Implement fetching all courses logic
+    Course.find().then(
+        courses => {
+            res.json(courses);
+    });
 });
 
 module.exports = router;
--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -5,10 +5,22 @@ const userMiddleware = require("../middleware/user");
 // User Routes
 router.post('/signup', (req, res) => {
     // Implement user signup logic
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    res.json({
+        message: "User created successfully"
+    })
 });
 
 router.get('/courses', (req, res) => {
     // Implement listing all courses logic
+    Course.find().then(
+        courses => {
+            res.json(courses);
+    });
 });
 
 router.post('/courses/:courseId', userMiddleware, (req, res) => {
