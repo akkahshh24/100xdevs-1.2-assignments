0567ed710d87abae9af0d871fe9f0c19763dc956
akkahshh.a <akkahshh.a@npci.org.in>
2024-01-08 11:43:30 +0530

    re-rendering

--- week-6/6.1-classwork/src/App.jsx
+++ week-6/6.1-classwork/src/App.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react"
+
 function App() {
 
   return (
     <>
-      <Header title="India"></Header>
+      <HeaderWithButton></HeaderWithButton>
       <Header title="Maldives"></Header>
     </>
   )
+}
+
+// one wat to optimize the code i.e. minize the number of re-renders
+// push the state down
+// adding this component to minimize the re-renders in our app 
+function HeaderWithButton() {
+  const [title, setTitle] = useState("India");
+
+  function updateTitle() {
+    setTitle("Random number " + Math.random())
+  }
+
+  return (
+    <>
+      <button onClick={updateTitle}>Click me to change title 1</button>
+      <Header title={title}></Header>
+    </>
+  )
+
 }
 
 function Header({title}) {
